0fe27f43d61e1f373a771085268eaf92afd30a72
Swapna <swapna_rv@hotmail.com>
2023-01-29 22:58:31 +0530

    First commit

---
+++ src/test/java/TestNgMavenExampleTest.java
@@ -0,0 +1,11 @@
+
+
+import org.testng.annotations.Test;
+
+public class TestNgMavenExampleTest {
+
+    @Test
+    public void exampleOfTestNgMaven() {
+        System.out.println("This is TestNG-Maven Example");
+    }
+}
